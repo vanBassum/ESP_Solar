0c1f06dfaa1e923e15da811c497c2718ebd6ae96
Bas Visscher <bas.visscher24@gmail.com>
2023-02-25 15:43:50 +0100

    Working example

--- main/main.cpp
+++ main/main.cpp
@@ -1,90 +1,40 @@
-
-#include "freertos/FreeRTOS.h"
 #include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
-#include "esp_wifi.h"
-#include "esp_sntp.h"
-#include "nvs_flash.h"
-#include "driver/gpio.h"
-#include "ESP_log.h"
-#include "driver/uart.h"
-#include "driver/gpio.h"
-#include <string.h>
-#include <cjson.h>
-#include "lib/rtos/task.h"
-#include "lib/rtos/stream.h"
-#include "lib/rtos/timer.h"
-#include "lib/tcpip/tcpsocket.h"
-#include "lib/rtos/task.h"
-#include "ina226.h"
-#include <driver/spi_master.h>
+#include "esp_base.h"
+#include "esp_lvgl.h"
+#include "esp_com.h"
+#include "esp_drivers.h"
+#include "esp_log.h"
 #include "st7735.h"
-#include "fonts.h"
-#include "esp_pm.h"
-#include "ina3221.h"
+#include "rom/gpio.h"
 
 
-#define SSID			"vanBassum"
-#define PSWD			"pedaalemmerzak"
+using namespace ESP_Base;
+using namespace ESP_LVGL;
+using namespace ESP_Com;
+using namespace ESP_Drivers;
 
-#define SCL 			GPIO_NUM_22
-#define SDA				GPIO_NUM_21
+#define SSID				"vanBassum"
+#define PSWD				"pedaalemmerzak"
 
+#define I2C_SCL 		  	GPIO_NUM_22
+#define I2C_SDA			  	GPIO_NUM_21
+
+#define SPI_CLK_Pin       	GPIO_NUM_27
+#define SPI_MOSI_Pin      	GPIO_NUM_14
+#define SPI_MISO_Pin      	GPIO_NUM_NC
+
+#define DISPLAY_RES_Pin     GPIO_NUM_25
+#define DISPLAY_DC_Pin      GPIO_NUM_13
+#define DISPLAY_CS_Pin      GPIO_NUM_26
+
+
+
+const char* TAG = "Main";
 
 extern "C" {
 	void app_main();
 }
 
-esp_err_t event_handler(void* ctx, system_event_t* event)
-{
-	return ESP_OK;
-}
-
-void StartWIFI()
-{
-	tcpip_adapter_init();
-	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-
-	wifi_config_t sta_config = { };
-	memcpy(sta_config.sta.ssid, SSID, strlen(SSID));
-	memcpy(sta_config.sta.password, PSWD, strlen(PSWD));
-	sta_config.sta.bssid_set = false;
-	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
-	ESP_ERROR_CHECK(esp_wifi_start());
-	ESP_ERROR_CHECK(esp_wifi_connect());
-	setenv("TZ", "UTC-1UTC,M3.5.0,M10.5.0/3", 1);
-	tzset();
-	sntp_setoperatingmode(SNTP_OPMODE_POLL);
-	sntp_setservername(0, "pool.ntp.org");
-	sntp_init();
-}
-
-static void i2c_master_init(int sda, int scl)
-{
-	static bool initialized = false;
-	if (!initialized)
-	{	
-		int i2c_master_port = 0;
-		i2c_config_t conf;
-		conf.mode = I2C_MODE_MASTER;
-		conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
-		conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
-		conf.master.clk_speed = 400000;
-		conf.sda_io_num = sda;
-		conf.scl_io_num = scl;
-		conf.clk_flags = 0;
-		i2c_param_config(i2c_master_port, &conf);
-		if (i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0) == ESP_OK)
-		{
-			initialized = true;
-		}
-	}
-}
 
 spi_device_handle_t handle;
 
@@ -92,29 +42,29 @@ void spi_master_init()
 {
 	esp_err_t ret;
 
-	gpio_pad_select_gpio(ST7735_CS_Pin);
-	gpio_set_direction(ST7735_CS_Pin, GPIO_MODE_OUTPUT);
-	gpio_set_level(ST7735_CS_Pin, 0);
+	gpio_pad_select_gpio(DISPLAY_CS_Pin);
+	gpio_set_direction(DISPLAY_CS_Pin, GPIO_MODE_OUTPUT);
+	gpio_set_level(DISPLAY_CS_Pin, 0);
 
 
-	gpio_pad_select_gpio(ST7735_DC_Pin);
-	gpio_set_direction(ST7735_DC_Pin, GPIO_MODE_OUTPUT);
-	gpio_set_level(ST7735_DC_Pin, 0);
+	gpio_pad_select_gpio(DISPLAY_DC_Pin);
+	gpio_set_direction(DISPLAY_DC_Pin, GPIO_MODE_OUTPUT);
+	gpio_set_level(DISPLAY_DC_Pin, 0);
 
 	
-	if (ST7735_RES_Pin >= 0) {
-		gpio_pad_select_gpio(ST7735_RES_Pin);
-		gpio_set_direction(ST7735_RES_Pin, GPIO_MODE_OUTPUT);
-		gpio_set_level(ST7735_RES_Pin, 0);
+	if (DISPLAY_RES_Pin >= 0) {
+		//gpio_pad_select_gpio(DISPLAY_RES_Pin);
+		gpio_set_direction(DISPLAY_RES_Pin, GPIO_MODE_OUTPUT);
+		gpio_set_level(DISPLAY_RES_Pin, 0);
 		vTaskDelay(pdMS_TO_TICKS(100));
-		gpio_set_level(ST7735_RES_Pin, 1);
+		gpio_set_level(DISPLAY_RES_Pin, 1);
 	}
 
 	
 	spi_bus_config_t buscfg = {
-		.mosi_io_num = ST7735_SDA_Pin,
-		.miso_io_num = -1,
-		.sclk_io_num = ST7735_SCL_Pin,
+		.mosi_io_num = SPI_MOSI_Pin,
+		.miso_io_num = SPI_MISO_Pin,
+		.sclk_io_num = SPI_CLK_Pin,
 		.quadwp_io_num = -1,
 		.quadhd_io_num = -1
 	};
@@ -124,7 +74,7 @@ void spi_master_init()
 
 	spi_device_interface_config_t devcfg =  {
 		.clock_speed_hz = 20000000,
-		.spics_io_num = ST7735_CS_Pin,
+		.spics_io_num = DISPLAY_CS_Pin,
 		.flags = SPI_DEVICE_NO_DUMMY,
 		.queue_size = 7,
 	};
@@ -158,13 +108,17 @@ void spi_master_write_byte(uint8_t* Data, size_t DataLength, int timeout)
 	//return true;
 }
 
-
-
 void app_main(void)
 {	
-	nvs_flash_init();
-	//StartWIFI();
-	
+	SPIBus spiBus;
+	ST7735 st7735;
+
+	INIT_AND_CONTINUE(TAG, "Initializing NVS", ESP32::InitNVS());
+	INIT_AND_CONTINUE(TAG, "Initializing WIFI", ESP32::InitWIFI(SSID, PSWD));
+	INIT_AND_CONTINUE(TAG, "Initializing NTP", ESP32::InitNTP());
+	INIT_AND_CONTINUE(TAG, "Initializing LVGL", LVGL::Init());
+
+
 	spi_master_init();
 	SetWritePinCallback(WrtPin);
 	SetTransmitCallback(spi_master_write_byte);
@@ -173,30 +127,30 @@ void app_main(void)
 	ST7735_FillScreen(0x00);
 
 	ST7735_WriteString(0, 0, "Hello", Font_11x18, 0xFFFF, 0x0000);
-
-	i2c_master_init(SDA, SCL);
-
-
-
-	INA3221 ina3221(INA3221_ADDR40_GND);
-	ina3221.begin();
-	ina3221.reset();
-	ina3221.setShuntRes(5, 5, 5);
-
-	char buf[128];
 	
-	TickType_t prev = xTaskGetTickCount();
-	while (true)
-	{
-		float current[3], voltage[3];
-		current[0] = ina3221.getCurrent(INA3221_CH1);
-		voltage[0] = ina3221.getVoltage(INA3221_CH1);
-		current[1] = ina3221.getCurrent(INA3221_CH2);
-		voltage[1] = ina3221.getVoltage(INA3221_CH2);
-		current[2] = ina3221.getCurrent(INA3221_CH3);
-		voltage[2] = ina3221.getVoltage(INA3221_CH3);
+	//INIT_AND_CONTINUE(TAG, "Initializing SPI", spiBus.Init(HSPI_HOST, SPI_CLK_Pin, SPI_MOSI_Pin, SPI_MISO_Pin));
+//
+//
+	//st7735.settings.cs = DISPLAY_CS_Pin;
+	//st7735.settings.dc = DISPLAY_DC_Pin;
+	//st7735.settings.rst = DISPLAY_RES_Pin;
+	//st7735.Init(spiBus);
+//
+//
+//
+//
+	//for(int i=0; i<64; i++)
+	//{
+	//	st7735.ST7735_DrawPixel(i, i, 0x0000);
+	//	st7735.ST7735_DrawPixel(128-i, i, 0xFFFF);
+	//}
 
-		ESP_LOGI("MAIN", "%.2fV %.2fA   %.2fV %.2fA   %.2fV %.2fA", voltage[0], current[0], voltage[1], current[1], voltage[2], current[2]);
-		vTaskDelay(pdMS_TO_TICKS(250));
-	}	
+	while(1)
+	{
+		vTaskDelay(pdMS_TO_TICKS(100));
+	}
+
 }
+
+
+
